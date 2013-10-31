e5215c4ce2ea15cdf58c8af0943bca2e40cb0235
rich-b <rcbeav@yahoo.com>
2013-10-31 13:33:05 -0700

    launch in lower right

--- background.js
+++ background.js
@@ -1,8 +1,15 @@
 ﻿chrome.app.runtime.onLaunched.addListener(function () {
+	var screenWidth = screen.availWidth;
+	var screenHeight = screen.availHeight;
+	var width = 600;
+	var height = 200;
+
     chrome.app.window.create('tab.html', {
         'bounds': {
-            'width': 500,
-            'height': 500
+            width: width,
+            height: height,
+            left: Math.round((screenWidth-width) - 10),
+      		top: Math.round((screenHeight-height) - 30)
         }
     });
 });
