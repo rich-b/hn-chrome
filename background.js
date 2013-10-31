398796d7b3df18101eeee4cf01475983684ee17f
Chris Tesene <info@christesene.com>
2013-10-31 10:25:31 -0700

    working popup window with test message

---
+++ background.js
@@ -0,0 +1,8 @@
+﻿chrome.app.runtime.onLaunched.addListener(function () {
+    chrome.app.window.create('tab.html', {
+        'bounds': {
+            'width': 400,
+            'height': 500
+        }
+    });
+});
